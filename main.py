4232cc589683eee78050f6d9fef3307e05e12768
Maksym Bez <neburitoss@gmail.com>
2025-03-21 00:17:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFather
+load_dotenv()
+TOKEN = getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+
+    try:
+
+        await message.answer("Вітаю тебе в Конотопському вістнику! \n Я поки що юзлесс, тому послухай мій трек")
+        await message.answer("https://www.youtube.com/watch?v=uxs_HYw_mLk")
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
